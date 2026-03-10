a5d0e4599c4fb6ded35976c90cd1bfd537ffc5c2
yyyoungha <yyyoungha@gmail.com>
2026-03-10 00:25:26 +0000

    create SSM parameter in us-west-2 region

--- gateway/terraform/main.tf
+++ gateway/terraform/main.tf
@@ -4,3 +4,10 @@ resource "aws_ssm_parameter" "account_example" {
   type  = "String"
   value = "account-specific-value"
 }
+
+resource "aws_ssm_parameter" "account_example" {
+  name  = "/aft/account/us-west-2"
+  type  = "String"
+  value = "account-specific-value2"
+  provider = aws.us_west_2_provider
+}
