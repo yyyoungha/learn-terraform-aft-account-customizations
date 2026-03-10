c9149e01568a1c623dd88f3693d7ee0375b4169f
yyyoungha <yyyoungha@gmail.com>
2026-03-10 00:41:46 +0000

    fix resource name

--- gateway/terraform/main.tf
+++ gateway/terraform/main.tf
@@ -1,11 +1,11 @@
-# aft-account-customizations/MY_CUSTOMIZATION/terraform/main.tf
+# aft-account-customizations/gateway/terraform/main.tf
 resource "aws_ssm_parameter" "account_example" {
   name  = "/aft/account/example"
   type  = "String"
   value = "account-specific-value"
 }
 
-resource "aws_ssm_parameter" "account_example" {
+resource "aws_ssm_parameter" "account_example_us_west_2" {
   name  = "/aft/account/us-west-2"
   type  = "String"
   value = "account-specific-value2"
